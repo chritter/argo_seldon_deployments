84f4903c49e248d38c4e056f96a64128d907ac20
chritter <christian.h.ritter@gmail.com>
2021-08-24 07:11:51 -0400

    rename getting_started

--- getting_started/api_access/api_access_curl.sh
+++
@@ -1,8 +0,0 @@
-# port 9000 exposed through the sklearn container
-curl -s -X POST http://localhost:9000/api/v1.0/predictions \
-    -H 'Content-Type: application/json' \
-    -d '{ "data": { "ndarray": [[1,2,3,4]] } }'
-
-# port 8000 exposed through seldon-container-engine container
-# Swagger API doc at http://localhost:8000/api/v1.0/doc/
-

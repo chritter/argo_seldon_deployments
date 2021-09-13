ba3ddef8e8ca49738844c5802b2695e38f8afc96
chritter <christian.h.ritter@gmail.com>
2021-09-13 15:08:29 -0400

    update seldon-getting-started-custom

---
+++ seldon-getting-started-custom/start_microservice_local.sh
@@ -0,0 +1,4 @@
+# for local testing, start microsevice
+# I enable full debug mode
+# referencing Model.py via Model
+seldon-core-microservice Model  --service-type MODEL --debug DEBUG --log-level DEBUG 

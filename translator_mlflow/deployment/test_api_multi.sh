1deeda81c8c0713d8d775c69c92ea16593f5172e
chritter <christian.h.ritter@gmail.com>
2021-10-19 13:59:48 -0400

    updated text classifier

--- translator_mlflow/deployment/test_api_multi.sh
+++ translator_mlflow/deployment/test_api_multi.sh
@@ -3,5 +3,5 @@ for i in `seq 1 6000`; do \
    echo next request
    sleep 1 && curl -s -X POST http:///localhost:9000/api/v1.0/predictions \
     -H 'Content-Type: application/json' \
-    -d '{ "data": { "ndarray": ["Aujourd hui est un bon jour"] } }'
+    -d '{ "data": { "ndarray": ["Aujourd hui est un bon jour parce que nous utilisons une nouvelle API "] } }'
 done
